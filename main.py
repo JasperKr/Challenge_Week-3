935a212a002ba34d9b1e34917499731bcda023e5
JasperKr <j.krocke@outlook.com>
2023-11-27 14:47:47 +0100

    fix daan commit want daan

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ import classes
 
 pygame.init()
 screen = pygame.display.set_mode(
-    (1280, 720))
+    (1280, 720), pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE)
 pygame.display.set_caption("HOI DAAN")
 clock = pygame.time.Clock()
 
-# shader = pygame_shaders.Shader(size=(1280, 720), display=(1280, 720),
-#    pos=(0, 0), vertex_path="shaders/vertex.glsl",
-#    fragment_path="shaders/fragment.glsl")  # Load your shader!
+shader = pygame_shaders.Shader(size=(1280, 720), display=(1280, 720),
+                               pos=(0, 0), vertex_path="shaders/vertex.glsl",
+                               fragment_path="shaders/fragment.glsl", target_texture=screen)  # Load your shader!
 
-
-# Create a new surface, this will be where you do all your pygame rendering
-# display = pygame.Surface((1280, 720))
-# display.set_colorkey((0, 0, 0))  # Make all black on the display transparent
 
 pygame.init()
 
@@ -34,12 +30,6 @@ def color(r=0, g=0, b=0):
     return (r * 255, g * 255, b * 255)
 
 
-def draw(screen, real_rgb_color):
-    screen.fill(color(math.cos(time.time()) * 0.5 + 0.5,
-                math.sin(time.time()) * 0.5 + 0.5, 0.5))
-    pygame.draw.rect(screen, real_rgb_color, (100, 100, 500, 500), 2)
-
-
 running = True
 while running:
     for event in pygame.event.get():
@@ -49,7 +39,7 @@ while running:
     draw(screen)
 
     # Render the display onto the OpenGL display with the shaders!
-    # shader.render(display)
+    shader.render(screen)
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
