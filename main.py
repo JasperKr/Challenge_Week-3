8b08c4a658a692e3135c8a105d48d75e1a45d430
000Tiger0001 <1077100@hr.nl>
2023-11-27 14:46:20 +0100

    hoi[]

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ import time
 
 pygame.init()
 screen = pygame.display.set_mode(
-    (1280, 720), pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE)
+    (1280, 720))
 pygame.display.set_caption("HOI DAAN")
 clock = pygame.time.Clock()
 
-shader = pygame_shaders.Shader(size=(1280, 720), display=(1280, 720),
-                               pos=(0, 0), vertex_path="shaders/vertex.glsl",
-                               fragment_path="shaders/fragment.glsl", target_texture=screen)  # Load your shader!
+# shader = pygame_shaders.Shader(size=(1280, 720), display=(1280, 720),
+#    pos=(0, 0), vertex_path="shaders/vertex.glsl",
+#    fragment_path="shaders/fragment.glsl")  # Load your shader!
 
-
-pygame.init()
 
 # Create a new surface, this will be where you do all your pygame rendering
-display = pygame.Surface((1280, 720))
-display.set_colorkey((0, 0, 0))  # Make all black on the display transparent
+# display = pygame.Surface((1280, 720))
+# display.set_colorkey((0, 0, 0))  # Make all black on the display transparent
 
 # pygame_shaders.Shader.send(variable_name: str, data: List[float])
 
@@ -32,8 +30,7 @@ def color(r=0, g=0, b=0):
 def draw(screen, real_rgb_color):
     screen.fill(color(math.cos(time.time()) * 0.5 + 0.5,
                 math.sin(time.time()) * 0.5 + 0.5, 0.5))
-    pygame.draw.circle(screen, (real_rgb_color), [200, 200], 400, 50)
-    pygame.display.flip()
+    pygame.draw.rect(screen, real_rgb_color, (100, 100, 500, 500), 2)
 
 
 running = True
@@ -44,7 +41,8 @@ while running:
 
     draw(screen, dark_graaaaaaaaaaaaaaaaaaaaaaaaaaaay)
     # Render the display onto the OpenGL display with the shaders!
-    shader.render(display)
+    # shader.render(display)
+    pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
